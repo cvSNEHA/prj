6ec953018635a4fdd2d55adb15db847490a02707
cvSNEHA <snehasanthosh2000@gmail.com>
2024-01-17 09:10:38 +0530

    hello world

---
+++ test.py
@@ -0,0 +1,20 @@
+#1
+print('hello world')
+
+#2
+def fib(n):
+    a,b=0,1
+    while a<n:
+        print(a,end=' ')
+        a,b=b,a+b
+    print()
+
+fib(12)
+
+#3
+n=int(input('number='))
+a,b=0,1
+while a<n:
+    print(a,end=' ')
+    a,b=b,a+b
+print()
